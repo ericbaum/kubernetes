6299d2208393ad9ded0c9697b46b577eb6ef95c0
Eric Baum <ebaum@cpqd.com.br>
2017-10-06 10:58:47 -0300

    Adds route and first user configuration

---
+++ config_scripts/kong.config.sh
@@ -0,0 +1,175 @@
+#!/bin/bash
+
+echo "Waiting for kong to be up"
+
+# ---  0.11.x
+kong="http://kong:8001"
+
+# Wait for kong to be up
+until $(curl --output /dev/null --silent --head --fail ${kong}); do
+    printf '.'
+    sleep 2
+done
+
+echo
+
+echo "Kong is up, configuring routes"
+
+authConfig() {
+  curl -o /dev/null -sS -X POST $kong/apis/$1/plugins -d "name=jwt" # -d "config.claims_to_verify=exp"
+  curl -o /dev/null -sS -X POST $kong/apis/$1/plugins -d "name=pepkong" -d "config.pdpUrl=http://pdp-ws:9763"
+}
+
+# remove all previously configured apis from gateway
+for i in $(curl -sS ${kong}/apis  | grep -oP '(?<="id":")[a-f0-9-]+(?=")'); do
+  curl -o /dev/null -sS -X DELETE $kong/apis/$i
+done
+
+(curl -o /dev/null $kong/apis -sS -X PUT \
+    --header "Content-Type: application/json" \
+    -d @- ) <<PAYLOAD
+{
+    "name": "gui",
+    "uris": "/",
+    "strip_uri": false,
+    "upstream_url": "http://gui:80"
+}
+PAYLOAD
+# no auth: serves only static front-end content
+
+(curl -o /dev/null $kong/apis -sS -X POST \
+    --header "Content-Type: application/json" \
+    -d @- ) <<PAYLOAD
+{
+    "name": "metric",
+    "uris": "/metric",
+    "strip_uri": true,
+    "upstream_url": "http://orion:1026"
+}
+PAYLOAD
+authConfig "metric"
+
+
+(curl -o /dev/null $kong/apis -s -S -X POST \
+    --header "Content-Type: application/json" \
+    -d @- ) <<PAYLOAD
+{
+    "name": "template",
+    "uris": "/template",
+    "strip_uri": false,
+    "upstream_url": "http://devm:5000"
+}
+PAYLOAD
+authConfig "template"
+
+
+(curl -o /dev/null $kong/apis -s -S -X POST \
+    --header "Content-Type: application/json" \
+    -d @- ) <<PAYLOAD
+{
+    "name": "device",
+    "uris": "/device",
+    "strip_uri": false,
+    "upstream_url": "http://devm:5000"
+}
+PAYLOAD
+authConfig "device"
+
+(curl -o /dev/null $kong/apis -s -S -X POST \
+    --header "Content-Type: application/json" \
+    -d @- ) <<PAYLOAD
+{
+    "name": "auth-service",
+    "uris": "/auth",
+    "strip_uri": true,
+    "upstream_url": "http://auth:5000"
+}
+PAYLOAD
+# no auth: this is actually the endpoint used to get a token
+# rate plugin limit to avoid brute-force atacks
+curl -o /dev/null -sS -X POST ${kong}/apis/auth-service/plugins \
+    --data "name=rate-limiting" \
+    --data "config.minute=5" \
+    --data "config.hour=40" \
+    --data "config.policy=local"
+
+
+# revoke all tokens: maintence only API
+(curl -o /dev/null $kong/apis -s -S -X POST \
+    --header "Content-Type: application/json" \
+    -d @- ) <<PAYLOAD
+{
+    "name": "auth-revoke",
+    "uris": "/auth/revoke",
+    "strip_uri": true,
+    "upstream_url": "http://auth:5000/auth/revoke"
+}
+PAYLOAD
+curl -o /dev/null -sS -X POST  $kong/apis/auth-revoke/plugins \
+    --data "name=request-termination" \
+    --data "config.status_code=403" \
+    --data "config.message=Not authorized"
+
+
+(curl -o /dev/null $kong/apis -s -S -X POST \
+    --header "Content-Type: application/json" \
+    -d @- ) <<PAYLOAD
+{
+    "name": "user-service",
+    "uris": "/auth/user",
+    "strip_uri": true,
+    "upstream_url": "http://auth:5000/user"
+}
+PAYLOAD
+authConfig "user-service"
+
+(curl -o /dev/null $kong/apis -s -S -X POST \
+    --header "Content-Type: application/json" \
+    -d @- ) <<PAYLOAD
+{
+    "name": "flows",
+    "uris": "/flows",
+    "strip_uri": true,
+    "upstream_url": "http://mashup:3000"
+}
+PAYLOAD
+authConfig "flows"
+
+(curl -o /dev/null $kong/apis -s -S -X POST \
+    --header "Content-Type: application/json" \
+    -d @- ) <<PAYLOAD
+{
+    "name": "history",
+    "uris": "/history",
+    "strip_uri": true,
+    "upstream_url": "http://sth:8666"
+}
+PAYLOAD
+authConfig "history"
+
+# TODO it might be a good idea to merge this with the orchestrator itself
+(curl -o /dev/null $kong/apis -s -S -X POST \
+    --header "Content-Type: application/json" \
+    -d @- ) <<PAYLOAD
+{
+    "name": "mashup",
+    "uris": "/mashup",
+    "strip_uri": true,
+    "upstream_url": "http://mashup:1880"
+}
+PAYLOAD
+# no auth: serves only available types
+
+(curl -o /dev/null $kong/apis -s -S -X POST \
+    --header "Content-Type: application/json" \
+    -d @- ) <<PAYLOAD
+{
+    "name": "httpDevices",
+    "uris": "/iot",
+    "strip_uri": false,
+    "upstream_url": "http://iotagent:8080"
+}
+PAYLOAD
+# no auth: used for middleware <-> device communication via HTTP(s)
+
+echo "API Gateway Paths configured"
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,17 @@
-#!/bin/sh
+#!/bin/bash
+
+# Check if parameters are set
+if [ $# -lt 1 ]; then
+    echo "Error: The external ip parameter is required\n"
+    echo "./deploy.sh AA.BB.CC.DD"
+    exit
+fi
+
+# Read the external ip
+EXTERNAL_IP=$1
+
+# One-time update the external ip on the yaml files
+sed -i "s/\[EXTERNAL_IP\]/${EXTERNAL_IP}/g" manifests/apigw.yaml
 
 # Create the dojot namespace
 kubectl create namespace dojot
@@ -18,8 +31,15 @@ kubectl create -n dojot -f manifests/mashup.yaml
 kubectl create -n dojot -f manifests/mongo.yaml
 kubectl create -n dojot -f manifests/mqtt.yaml
 kubectl create -n dojot -f manifests/orion.yaml
-kubectl create -n dojot -f manifests/pdp.yaml
 kubectl create -n dojot -f manifests/perseo-core.yaml
 kubectl create -n dojot -f manifests/perseo-fe.yaml
 kubectl create -n dojot -f manifests/postgres.yaml
 kubectl create -n dojot -f manifests/sth.yaml
+
+kubectl create -n dojot configmap kong-route-config --from-file=config_scripts/kong.config.sh
+kubectl create -n dojot configmap create-admin-user --from-file=config_scripts/create-admin-user.sh
+
+kubectl create -n dojot -f manifests/config-jobs.yaml
+
+# Changes the external ip back to a tag
+sed -i "s/${EXTERNAL_IP}/\[EXTERNAL_IP\]/g" manifests/apigw.yaml
