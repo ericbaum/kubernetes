608d213b7fb6a43333cc1211227b37a4997a979c
Eric Baum <ebaum@cpqd.com.br>
2018-01-31 16:51:02 -0200

    Adds persister to the kubernetes deployment

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,7 @@ kubectl create -f manifests/EXTERNAL_ACCESS/public-ip.yaml
 kubectl create -n dojot configmap iotagent-conf --from-file=iotagent/config.js
 kubectl create -n dojot configmap kong-route-config --from-file=config_scripts/kong.config.sh
 kubectl create -n dojot configmap create-admin-user --from-file=config_scripts/create-admin-user.sh
+kubectl create -n dojot configmap persister-conf --from-file=persister_conf/history.mongodb.conf
 
 # Instantiate SAs and roles before starting the dependent services
 kubectl create -f manifests/SAS_AND_ROLES/
